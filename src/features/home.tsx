42fb31821cd74a53d5562d60f306dcb1559f1d75
Alex A <alex.andsten@gmail.com>
2024-12-04 15:31:57 +0100

    logo + MUI

--- src/features/home.tsx
+++ src/features/home.tsx
@@ -7,26 +7,20 @@ function Home() {
 
   return (
     <>
-      <div>
-       
-      </div>
       <Stack flexDirection={'row'} width={'100%'} gap={3}>
         <img style={{ height: '4em', width: '4em' }} src={AlexVivereLogo}/>
         <h1>Home</h1>
       </Stack>
-      <div className="card">
         
+      <Stack flexDirection={'row'} width={'100%'} gap={3}>
         <Link to="/" style={{ textDecoration: 'none' }}>
             Home
         </Link>
         <Link to="/meditation" style={{ textDecoration: 'none' }}>
            Meditation
         </Link>
-        <Typography sx={{ fontSize: '3em'}}>Hello world</Typography>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </Stack>
+      <Typography sx={{ fontSize: '3em', fontFamily: 'Major Mono Display'}}>Hello world</Typography>
     </>
   )
 }
